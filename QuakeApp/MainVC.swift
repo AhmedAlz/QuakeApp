a04dabfcfb1aef90512542f55a8111ee2f2f2dc6
Ahmed <a8888h@gmail.com>
2018-02-27 21:45:30 -0800

    online NTP clock

--- QuakeApp/MainVC.swift
+++ QuakeApp/MainVC.swift
@@ -28,6 +28,7 @@ class MainVC: UIViewController {
     var duration : Double = 10
     var counter : Double = 1
     var startRecording = false
+    var manualStart = false
     let samplingRate : Double = 1/100.0
     let url = "http://ec2-54-153-50-104.us-west-1.compute.amazonaws.com/api"
     
@@ -65,9 +66,14 @@ class MainVC: UIViewController {
         self.settings.text = " Threshould = \((threshold * 100.0).rounded()/100)  m/s^2 \n Duration = \((duration * 100.0).rounded()/100) seconds"
         
         
-        self.startupdate()
+        Clock.sync { date, offset in
+            print(date?.timeIntervalSince1970 ?? 0.0)
+             self.startupdate()
+        }
+        
         
     }
+    
     
     func startupdate()  {
         
@@ -77,32 +83,33 @@ class MainVC: UIViewController {
         var t = [Any]()
         var readings = [Any]()
         
-//        var date = NSDate()
-        var now  = NSDate().timeIntervalSince1970 ?? 0.0
-        print(now)
 
-//        var now = Clock.now?.timeIntervalSince1970 ?? 0.0
-//        print(now)
+        //var now  = NSDate().timeIntervalSince1970 ?? 0.0
+        //print(now)
+
+        var now = Clock.now?.timeIntervalSince1970 ?? 0.0
+        print(now)
         if manager.isAccelerometerAvailable {
             manager.accelerometerUpdateInterval = samplingRate
             manager.startAccelerometerUpdates(to: OperationQueue.main) {
                 [weak self] data, error in
-                now = now + self!.samplingRate
+                //now = now + self!.samplingRate
                 
-                self!.readings.text = "x: \((data!.acceleration.x * 9.80665 * 100.0).rounded()/100)\ny: \((data!.acceleration.y * 9.80665 * 100).rounded()/100.0) \nz: \((data!.acceleration.z * 9.80665 * 100.0).rounded()/100) \n time: \(now)"
+                self!.readings.text = "x: \((data!.acceleration.x * 9.80665 * 100.0).rounded()/100)\ny: \((data!.acceleration.y * 9.80665 * 100).rounded()/100.0) \nz: \((data!.acceleration.z * 9.80665 * 100.0).rounded()/100) \n time: \(Clock.now?.timeIntervalSince1970 ?? 0.0)"
                 
-                if abs(data!.acceleration.z * 9.80665) > 8.0 && sqrt(pow(data!.acceleration.x * 9.80665,2) + pow(data!.acceleration.y * 9.80665,2)) > Double((self?.threshold)!) && self!.counter == 1  {
+                if (abs(data!.acceleration.z * 9.80665) > 8.0 && sqrt(pow(data!.acceleration.x * 9.80665,2) + pow(data!.acceleration.y * 9.80665,2)) > Double((self?.threshold)!) && self!.counter == 1) || (self!.manualStart == true && self!.counter == 1) {
                     self?.startRecording = true
                 }
                 
                 if self!.startRecording {
+                    self!.manualStart = false
                     if self!.counter == 1 {
-                        now = NSDate().timeIntervalSince1970
-                        print(now)
+                        //now = NSDate().timeIntervalSince1970
+                        print(Clock.now?.timeIntervalSince1970 ?? 0.0)
                         print("start")
                     } else if self!.counter ==  (self!.duration * (1/self!.samplingRate )) - 2.0 {
-                        now = NSDate().timeIntervalSince1970
-                        print(now)
+                        //now = NSDate().timeIntervalSince1970
+                        print(Clock.now?.timeIntervalSince1970 ?? 0.0)
                         print("end")
                     }
                     
@@ -111,16 +118,16 @@ class MainVC: UIViewController {
                         x.append(data!.acceleration.x * 9.80665)
                         y.append(data!.acceleration.y * 9.80665)
                         z.append(data!.acceleration.z * 9.80665)
-                        t.append(now)
+                        t.append(Clock.now?.timeIntervalSince1970 ?? 0.0)
                         
                     } else if self!.counter == self!.duration * (1/self!.samplingRate )  {
                         let readings = [  "x": x , "y": y , "z": z, "rtime": t]
                         let parameters: Parameters = ["deviceID": self!.emailAddress , "buildingNumber" : self!.buildingNumber ,"streetName" : self!.streetName ,"zipCode" : self!.zipCode ,"floor" : self!.floor, "x": x , "y": y , "z": z, "rtime": t, "reading" : readings]
-                        
+                        // add an if statment to save the data in the device and try to send later if there is now internet connection at the time detection
                         Alamofire.request(self!.url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
                             .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
                                 print("Progress: \(progress.fractionCompleted)")
-                                print(now)
+                                print(Clock.now?.timeIntervalSince1970 ?? 0.0)
                                 print("sent")
                                 
                         }
@@ -131,6 +138,7 @@ class MainVC: UIViewController {
                         z = []
                         t = []
                         self!.startRecording = false
+                        
                     }
                 }
                 
@@ -141,6 +149,10 @@ class MainVC: UIViewController {
         
     }
     
+    @IBAction func manualStartButton(_ sender: Any) {
+        self.manualStart = true
+        
+    }
     
     
     
