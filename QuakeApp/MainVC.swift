a8d78c1afdfcf4b89c96d32cd20624b643518a28
Ahmed <a8888h@gmail.com>
2018-03-05 02:59:57 -0800

    stopAcc error

--- QuakeApp/MainVC.swift
+++ QuakeApp/MainVC.swift
@@ -157,6 +157,23 @@ class MainVC: UIViewController {
         }
         
     }
+    @IBAction func updateAdressPressed(_ sender: Any) {
+        if manager.isAccelerometerActive{
+        manager.stopAccelerometerUpdates()
+        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "updateAddressID")
+        self.present(vc, animated: true)
+    }
+    
+    @IBAction func changeSettingPressed(_ sender: Any) {
+        if manager.isAccelerometerActive{
+            manager.stopAccelerometerUpdates()
+        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "settingsID")
+        self.present(vc, animated: true)
+    }
     
     @IBAction func manualStartButton(_ sender: Any) {
         self.manualStart = true
